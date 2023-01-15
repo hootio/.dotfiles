5ed4b7541c846c7c34c6d44d9803f823b89d3180
Hootan Shadmehr <hootan@umich.edu>
2023-01-15 00:34:48 -0800

    Fix .init.sh not doing exec zsh

--- .init.sh
+++ .init.sh
@@ -3,7 +3,7 @@
 # Sets up .dotfiles on new machine
 #
 # Usage:
-#   curl https://raw.githubusercontent.com/hootio/.dotfiles/main/.init.sh | bash
+#   bash <(curl https://raw.githubusercontent.com/hootio/.dotfiles/main/.init.sh)
 
 mkdir -p $HOME/github
 git clone --bare git@github.com:hootio/.dotfiles.git $HOME/github/.dotfiles
@@ -11,4 +11,3 @@ config="git --git-dir=$HOME/github/.dotfiles/ --work-tree=$HOME"
 $config config --local status.showUntrackedFiles no
 $config checkout
 exec zsh
-source $HOME/.zshrc
