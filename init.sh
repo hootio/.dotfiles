46e2efd3c20e192d7fbb62b5aadde5e38c8973bb
Hootan Shadmehr <hootan@umich.edu>
2023-01-14 22:18:41 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#
+# Sets up .dotfiles on new machine
+#
+# Usage:
+#   curl https://raw.githubusercontent.com/hootio/.dotfiles/main/init.sh | bash
+
 mkdir -p $HOME/github
 git clone --bare git@github.com:hootio/.dotfiles.git $HOME/github/.dotfiles
 config="git --git-dir=$HOME/github/.dotfiles/ --work-tree=$HOME"
