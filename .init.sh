89f0e3b11da99f41ea8d2d822460a8679397eda1
Hootan Shadmehr <hootan@umich.edu>
2023-01-16 01:34:06 -0800

    Add Brewfile

--- .init.sh
+++ .init.sh
@@ -5,9 +5,19 @@
 # Usage:
 #   bash <(curl https://raw.githubusercontent.com/hootio/.dotfiles/main/.init.sh)
 
+# set up local .dotfiles repo
 mkdir -p $HOME/github
 git clone --bare git@github.com:hootio/.dotfiles.git $HOME/github/.dotfiles
 config="git --git-dir=$HOME/github/.dotfiles/ --work-tree=$HOME"
 $config config --local status.showUntrackedFiles no
 $config checkout
+
+# brew
+## install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+## install brew packages
+brew bundle --file $HOME/Brewfile --no-lock
+brew bundle check
+
+# reload zsh
 exec zsh
