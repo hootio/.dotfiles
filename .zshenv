9f575b33b3f8a6fb4376a994cbe39648327b5a47
Hootan Shadmehr <hootan@umich.edu>
2025-12-07 16:28:22 -0800

    Remove linux support - will use feature, dnf, and dotsync2 on work remote machines and manually keep .zshrc in relative sync

--- .zshenv
+++ .zshenv
@@ -5,7 +5,3 @@ else
   export PATH="$HOME/.cargo/bin:$PATH"
 fi
 
-# brew on linux
-if [ -d "$HOME/.brew" ]; then
-  export PATH="$HOME/.brew/bin:$HOME/.brew/sbin:$PATH"
-fi
--- init.sh
+++ init.sh
@@ -10,27 +10,20 @@ set -euo pipefail
 #
 
 OS_TYPE=$(uname -s)
-if [[ "$OS_TYPE" == "Darwin" ]]; then
-  PLATFORM="mac"
-elif [[ "$OS_TYPE" == "Linux" ]]; then
-  PLATFORM="linux"
-else
-  echo "Error: Unsupported OS type '$OS_TYPE'. Must be Darwin or Linux"
+if [[ "$OS_TYPE" != "Darwin" ]]; then
+  echo "Error: Unsupported OS type '$OS_TYPE'. Must be Darwin"
   exit 1
 fi
 
 # consts
-BREW_LINUX_PATH="$HOME/.brew"
 SSH_KEY_PATH="$HOME/.ssh/github_ed25519"
 REPO_DIR="$HOME/github/.dotfiles"
 config="git --git-dir=$REPO_DIR --work-tree=$HOME"
 
 cleanup() {
-  echo "An error occurred. Cleaning up..."
+  echo "Cleaning up..."
   echo "Deleting $HOME/.gitconfig"
   rm -f .gitconfig
-  echo "Deleting $BREW_LINUX_PATH"
-  rm -rf $BREW_LINUX_PATH
   $config ls-tree -z --name-only -r HEAD | xargs -0 -I{} sh -c 'echo "Deleting $HOME/{}"; rm -f "$HOME/{}"'
   echo "Deleting $REPO_DIR"
   rm -rf $REPO_DIR
@@ -42,8 +35,6 @@ if [[ "${1:-}" == "cleanup" ]]; then
   cleanup
   exit 0
 fi
-
-echo "Setting up dotfiles for platform: $PLATFORM"
 
 # ensure github ssh key exists
 if [ -f $SSH_KEY_PATH ]; then
@@ -67,23 +58,13 @@ else
   $config pull || { echo "Failed to pull latest changes."; exit 1; }
 fi
 
-# set up .gitconfig
-cat .gitconfig.template >> $HOME/.gitconfig
-
 # install tmux config
 curl -fsSL https://raw.githubusercontent.com/gpakosz/.tmux/master/.tmux.conf > $HOME/.config/tmux/tmux.conf
 
 # install brew
-if [ "$PLATFORM" = "mac" ]; then
-  BREWFILE=$HOME/Brewfile.mac
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  BREW_PREFIX=$(brew --prefix)
-elif [ "$PLATFORM" = "linux" ]; then
-  BREWFILE=$HOME/Brewfile.linux
-  BREW_PREFIX="$BREW_LINUX_PATH"
-  mkdir -p "$BREW_PREFIX"
-  curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C "$BREW_PREFIX"
-fi
+BREWFILE=$HOME/Brewfile
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+BREW_PREFIX=$(brew --prefix)
 eval "$($BREW_PREFIX/bin/brew shellenv)"
 
 # install brew packages
@@ -92,24 +73,9 @@ brew bundle check --file $BREWFILE
 brew update
 brew upgrade
 
-if [ "$PLATFORM" = "mac" ]; then
-  # install rustc and cargo
-  rustup-init -y
-elif [ "$PLATFORM" = "linux" ]; then
-  # sync everything across servers
-  ## .dotfiles repo
-  dotsync2 paths add "$HOME/github/.dotfiles"
-  TRACKED_FILES=$($config ls-tree --full-tree -r --name-only HEAD)
-  for file in $TRACKED_FILES; do
-    if [ -f "$HOME/$file" ]; then
-      dotsync2 paths add "$HOME/$file"
-    fi
-  done
-  ## brew and packages
-  dotsync2 paths add "$BREW_PREFIX"
-  ## push
-  dotsync2 push
-fi
+# install rustc and cargo
+rustup-init -y
 
 # reload zsh as login shell to run .zprofile
 exec zsh -l
+
