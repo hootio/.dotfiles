7d046c657ce3efaad8dd32d0ec4223c78dbb9fb7
Hootan Shadmehr <hootan@umich.edu>
2026-01-25 19:02:09 -0800

    Sync nvim options and add OSC 52 clipboard for SSH

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,34 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local opt = vim.opt
+
+-- show absolute numbers in insert mode, relative in normal mode
+opt.relativenumber = true
+vim.cmd([[
+  augroup numbertoggle
+    autocmd!
+    autocmd BufEnter,FocusGained,InsertLeave * set relativenumber
+    autocmd BufLeave,FocusLost,InsertEnter   * set norelativenumber
+  augroup END
+]])
+
+opt.hidden = true -- allow background buffers
+opt.joinspaces = false -- join lines without two spaces
+
+-- Tab complete for cmd mode should autocomplete the first result immediately.
+opt.wildmode = "full"
+
+-- OSC 52 clipboard support for remote SSH sessions
+-- This allows yanking to local clipboard over SSH
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
